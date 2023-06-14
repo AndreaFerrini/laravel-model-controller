27969958f1b32050808784f91fd9c4349611d004
AndreaFerrini <andrea.ferrini90@gmail.com>
2023-06-14 21:55:12 +0200

    modifiche estetiche

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -7,17 +7,17 @@
 @section ( "content" )
 
 <div class="row">
-            @foreach( $allMovies as $elem )
-                <div class="col-4">
-                    <div class="card">
-                        <img class="card-img-top" src="" alt="" srcset="">
-                        <div class="card-body text-center">
-                        <h2 class="mt-2">{{ $elem["title"] }}</h2>
-                        </div>
-                    </div>
+    @foreach( $allMovies as $elem )
+        <div class="col-4">
+            <div class="card">
+                <img class="card-img-top" src="" alt="" srcset="">
+                <div class="card-body text-center">
+                <h2 class="mt-2">{{ $elem["title"] }}</h2>
                 </div>
-            @endforeach
+            </div>
         </div>
+    @endforeach
+</div>
 
 
 
